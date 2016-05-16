fbe906b3929389c47fa2de9470915040aef3033e
Jalel Tounsi <jalel.tounsi@gmail.com>
2016-05-16 19:03:30 +0200

    two binding with ngModel
    first use of ngModel for the two-way binding

--- src/app/workouts.component.ts
+++ src/app/workouts.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
 
+export class Workout{
+    name: string;
+    difficulty: number;
+}
+
 @Component({
   moduleId: module.id,
   selector: 'workouts-app',
-  templateUrl: 'workouts.component.html',
+  template : `
+  <h1>{{title}}</h1>
+  <h2>Here goes the first workout, the {{workout.name}}</h2>
+  <div>
+  <label>name:</label>
+  <input [(ngModel)]="workout.name" placeholder="name">
+  </div>
+  <div>
+  <label>difficulty:</label>
+  <input [(ngModel)]="workout.difficulty" placeholder="difficulty">
+  </div>
+  `,
   styleUrls: ['workouts.component.css']
 })
 export class WorkoutsAppComponent {
-  title = 'workouts works!';
+  title = 'The mighty workouts list works!';
+  workout: Workout = {
+      name : 'Murph',
+      difficulty : 10
+  };
 }
